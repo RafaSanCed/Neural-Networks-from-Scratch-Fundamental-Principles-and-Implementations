8631ef498759a2a251d30e5fe3ec719506adff3d
RafaSanCed <rafael.sanchez53099@gmail.com>
2022-09-11 18:59:44 -0500

    Subir archivos necesarios para el funcionamiento de la red
    Aquí se cargaron en la carpeta los archivos necesarios para el funcionamiento de la red. Y se realizó una primera prueba.

---
+++ Prueba 1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 11 17:36:35 2022
+
+@author: rafae
+"""
+
+import mnist_loader
+import network
+import pickle
+training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
+training_data = list(training_data)
+test_data = list(test_data)
+net=network.Network([784,30,10])
+net.SGD( training_data, 30, 10, 3.0, test_data=test_data)
+archivo = open("red_prueba1.pkl",'wb')
+pickle.dump(net,archivo)
+archivo.close()
