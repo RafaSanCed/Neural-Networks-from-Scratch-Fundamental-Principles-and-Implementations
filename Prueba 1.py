6111ab6269adbd6bd9732958ce1cdded0bb6c770
RafaSanCed <rafael.sanchez53099@gmail.com>
2022-09-11 23:37:50 -0500

    Otro optimizador
    Intentamos poner el optimizador de SGD con inercia, aunque los resultados empeoraron.

--- Prueba 1.py
+++ Prueba 1.py
@@ -6,13 +6,13 @@ Created on Sun Sep 11 17:36:35 2022
 """
 
 import mnist_loader
-import network
+import network_SGD_fric
 import pickle
 training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 test_data = list(test_data)
-net=network.Network([784,30,30,30,10])
-net.SGD( training_data, 100, 100, 0.5, test_data=test_data)
+net=network_SGD_fric.Network([784,30,10])
+net.SGD_fric( training_data, 10, 10, 0.5, test_data=test_data)
 archivo = open("red_prueba1.pkl",'wb')
 pickle.dump(net,archivo)
 archivo.close()
