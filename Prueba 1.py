5bf5a78f033d8725762869b46a0a920e20960034
RafaSanCed <rafael.sanchez53099@gmail.com>
2022-09-12 00:02:39 -0500

    Cambio a tanh
    De la misma forma, al hacer el cambio a tanh, la red se vuelve aleatoria.

--- Prueba 1.py
+++ Prueba 1.py
@@ -8,10 +8,13 @@ Created on Sun Sep 11 17:36:35 2022
 import mnist_loader
 import network_SGD_fric
 import pickle
+import network
+import network_tanh
+
 training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 test_data = list(test_data)
-net=network_SGD_fric.Network([784,30,10])
+net=network_tanh.Network([784,30,10])
 net.SGD( training_data, 10, 10, 0.5, test_data=test_data)
 archivo = open("red_prueba1.pkl",'wb')
 pickle.dump(net,archivo)
--- network_tanh.py
+++ network_tanh.py
@@ -1,7 +1,13 @@
-# %load network.py
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 11 23:58:52 2022
+
+@author: rafae
+"""
+
 
 """
-network_SGD_fric.py
+network.py
 ~~~~~~~~~~
 IT WORKS
 
@@ -41,7 +47,7 @@ class Network(object):
     def feedforward(self, a):
         """Return the output of the network if ``a`` is input."""
         for b, w in zip(self.biases, self.weights):
-            a = ReLu(np.dot(w, a)+b)
+            a = tanh(np.dot(w, a)+b)
         return a
 
     def SGD(self, training_data, epochs, mini_batch_size, eta,
@@ -68,49 +74,13 @@ class Network(object):
                 training_data[k:k+mini_batch_size]
                 for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
-                self.update_mini_batch_SGD(mini_batch, eta)
+                self.update_mini_batch(mini_batch, eta)
             if test_data:
                 print("Epoch {} : {} / {}".format(j,self.evaluate(test_data),n_test))
             else:
                 print("Epoch {} complete".format(j))
-                
-    def update_mini_batch_SGD(self, mini_batch, eta):
-        """Update the network's weights and biases by applying
-        gradient descent using backpropagation to a single mini batch.
-        The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
-        is the learning rate."""
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
-        for x, y in mini_batch:
-            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w-(eta/len(mini_batch))*nw
-                        for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b-(eta/len(mini_batch))*nb
-                       for b, nb in zip(self.biases, nabla_b)]
-    
-    def SGD_fric(self, training_data, epochs, mini_batch_size, eta,
-             test_data=None):
-        training_data=list(training_data)
-        n = len(training_data)        
-        if test_data:
-            test_data = list(test_data)
-            n_test = len(test_data)
-        
-        for j in range(epochs):
-            random.shuffle(training_data)
-            mini_batches = [
-                training_data[k:k+mini_batch_size]
-                for k in range(0, n, mini_batch_size)]
-            for mini_batch in mini_batches:
-                self.update_mini_batch_SGD_fric(mini_batch, eta)
-            if test_data:
-                print("Epoch {} : {} / {}".format(j,self.evaluate(test_data),n_test))
-            else:
-                print("Epoch {} complete".format(j))
-    
-    def update_mini_batch_SGD_fric(self, mini_batch, eta):
+
+    def update_mini_batch(self, mini_batch, eta):
         """Update the network's weights and biases by applying
         gradient descent using backpropagation to a single mini batch.
         The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
@@ -140,11 +110,11 @@ class Network(object):
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, activation)+b
             zs.append(z)
-            activation = sigmoid(z)
+            activation = tanh(z)
             activations.append(activation)
         # backward pass
         delta = self.cost_derivative(activations[-1], y) * \
-            ReLu(zs[-1])
+            tanh_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
         # Note that the variable l in the loop below is used a little
@@ -155,7 +125,7 @@ class Network(object):
         # that Python can use negative indices in lists.
         for l in range(2, self.num_layers):
             z = zs[-l]
-            sp = ReLu(z)
+            sp =tanh_prime(z)
             delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
             nabla_b[-l] = delta
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
@@ -184,7 +154,10 @@ def sigmoid_prime(z):
     """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
 
-def ReLu(z):
+def tanh(z):
     """Derivative of the sigmoid function."""
     return np.tanh(z)
 
+def tanh_prime(z):
+    """Derivative of the sigmoid function."""
+    return (-np.tanh(z))
